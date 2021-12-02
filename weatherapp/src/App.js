ff9405b4f7976dd9c7ec1f61ee6bd1e458aeb607
Lolazy <55079453+AZ1X@users.noreply.github.com>
2021-12-02 07:49:20 +0000

    New

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -6,23 +6,27 @@ function App() {
     <div className="App">
   <>
   <Navbar bg="warning" variant="light">
-    <Container>
+    <Container fluid >
     <Navbar.Brand href="#home"><img
           alt=""
-          src="https://github.com/AZ1X/React-Weather-App-V2/blob/59b6a44c745075b0c3647b6889f919bb1a18de61/weatherapp/public/icons/sunny.png"
-          width="30"
-          height="30"
+          src="icons/sunny.png"
+          width="32"
+          height="32"
           className="d-inline-block align-top"
         />{' '}Weather App</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#home">None</Nav.Link>
+      <Nav.Link href="#features">Contact</Nav.Link>
     </Nav>
     </Container>
-  </Navbar>
+  </Navbar>       
   </>
-    </div>
+  <br />
+  <Container style={{padding:20, bottom:20}}>
+  <h1>Page</h1>
+  </Container>
+  <br />
+  </div>
   );
 }
 
