57a2fae719607cea1fb2d88fde5f6fd7f1e6005c
Lolazy <55079453+AZ1X@users.noreply.github.com>
2021-12-02 10:43:33 +0000

    New

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,4 +1,4 @@
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {Navbar, Nav, Container,} from 'react-bootstrap';
 import Page from './components/Page';
 
 function App() {
@@ -6,24 +6,25 @@ function App() {
     <div>
   <>
   <Navbar bg="warning" variant="light">
-    <Container fluid >
+    <Container fluid>
     <Navbar.Brand href="#home"><img
           alt=""
           src="icons/sunny.png"
-          width="32"
-          height="32"
+          width="30"
+          height="30"
           className="d-inline-block align-top"
-        />{' '}Weather App</Navbar.Brand>
-    <Nav className="me-auto">
+        />{' '}<strong>LLZ Weather</strong></Navbar.Brand>
+    <Nav className="ml-auto">
       <Nav.Link href="#home">None</Nav.Link>
       <Nav.Link href="#features">Contact</Nav.Link>
     </Nav>
+  
     </Container>
   </Navbar>       
   </>
   <br />
   <div className="App">
-    <Container style={{padding:20, bottom:20}}>
+    <Container style={{padding:15, bottom:20}}>
       <Page />
     </Container>
   </div>
--- weatherapp/src/components/Page/Page.jsx
+++ weatherapp/src/components/Page/Page.jsx
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react';
-
 import Header from '../Header';
+import styles from './Page.module.css';
+
+import From from '../From';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
 
 const Page = () => {
     return (
-        <h1>Page 1</h1>
+        <Fragment>
+            <Header />
+        <div className={styles.box}>
+          {/* From */}
+          {/* Error */}
+          {/* Loader */}
+          {/* Forecast */}
+        </div>
+        </Fragment>
     );
 };
 
